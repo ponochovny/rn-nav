9d26f5b84d8b9a5c4617878c69d2ab6daca6c73f
ponochovny <ponochovny@gmail.com>
2020-08-28 15:42:10 +0300

    Navigation styles

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import {
+	View,
+	Text,
+	FlatList,
+	StyleSheet,
+	TouchableOpacity,
+} from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data';
-
-const renderGridItem = (itemData) => {
-	return (
-		<View>
-			<Text>{itemData.item.title}</Text>
-		</View>
-	);
-};
+import Colors from '../constants/Colors';
 
 const CategoriesScreen = (props) => {
+	const renderGridItem = (itemData) => {
+		return (
+			<TouchableOpacity
+				style={styles.gridItem}
+				onPress={() => {
+					props.navigation.navigate({
+						routeName: 'CategoryMeals',
+						params: {
+							categoryId: itemData.item.id,
+						},
+					});
+				}}
+			>
+				<View>
+					<Text>{itemData.item.title}</Text>
+				</View>
+			</TouchableOpacity>
+		);
+	};
+
 	return (
 		<FlatList
 			data={CATEGORIES}
@@ -26,6 +45,11 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center',
+	},
+	gridItem: {
+		flex: 1,
+		margin: 15,
+		height: 150,
 	},
 });
 
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 
+import { CATEGORIES } from '../data/dummy-data';
+import Colors from '../constants/Colors';
+
 const CategoryMealsScreen = (props) => {
+	const catId = props.navigation.getParam('categoryId');
+
+	const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+
 	return (
 		<View style={styles.screen}>
 			<Text>The Category Meal Screen!</Text>
+			<Text>{selectedCategory.title}</Text>
 			<Button
 				title="Go to Details"
 				onPress={() => {
@@ -19,6 +27,16 @@ const CategoryMealsScreen = (props) => {
 			/>
 		</View>
 	);
+};
+
+CategoryMealsScreen.navigationOptions = (navigationData) => {
+	const catId = navigationData.navigation.getParam('categoryId');
+
+	const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+
+	return {
+		headerTitle: selectedCategory.title,
+	};
 };
 
 const styles = StyleSheet.create({
