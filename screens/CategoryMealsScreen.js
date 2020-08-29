bedf79a276dcf7b716d945a3b02b220facf6e550
ponochovny <ponochovny@gmail.com>
2020-08-29 22:48:32 +0300

    Meal details screen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -2,13 +2,26 @@ import React from 'react';
 import { View, Text, FlatList, StyleSheet } from 'react-native';
 
 import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const CategoryMealsScreen = (props) => {
 	const renderMealItem = (itemData) => {
 		return (
-			<View>
-				<Text>{itemData.item.title}</Text>
-			</View>
+			<MealItem
+				title={itemData.item.title}
+				image={itemData.item.imageUrl}
+				duration={itemData.item.duration}
+				complexity={itemData.item.complexity}
+				affordability={itemData.item.affordability}
+				onSelectMeal={() => {
+					props.navigation.navigate({
+						routeName: 'MealDetail',
+						params: {
+							mealId: itemData.item.id,
+						},
+					});
+				}}
+			/>
 		);
 	};
 
@@ -20,7 +33,11 @@ const CategoryMealsScreen = (props) => {
 
 	return (
 		<View style={styles.screen}>
-			<FlatList data={displayedMeals} renderItem={renderMealItem} />
+			<FlatList
+				data={displayedMeals}
+				renderItem={renderMealItem}
+				style={{ width: '100%' }}
+			/>
 		</View>
 	);
 };
